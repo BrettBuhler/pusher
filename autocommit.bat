a3e961d2f7c514dad20a71bc9977af182baf92ef
BrettBuhler <107363334+BrettBuhler@users.noreply.github.com>
2024-08-07 06:36:42 -0600

    123

--- autocommit.bat
+++ autocommit.bat
@@ -1,3 +1,3 @@
 git add -u                               REM :: Only add tracked files
-git commit -m "green square good"      REM :: Commit all your added files
+git commit -m "push"      REM :: Commit all your added files
 git push origin master                   REM :: Branch (i.e. master)
